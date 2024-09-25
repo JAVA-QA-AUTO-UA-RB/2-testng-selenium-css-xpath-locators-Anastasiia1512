555a97c8168b5cc320da515b04a628bfdbe2e7cc
Anastasiia1512 <anastasiazuravleva900@gmail.com>
2024-09-25 15:41:17 +0200

    corrections_have_been_made

--- src/test/java/SeleniumTestngTest.java
+++ src/test/java/SeleniumTestngTest.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -16,9 +18,13 @@ import java.util.List;
 // browser variable is available here as it's inherited, so you'll have it available at any place
 public class SeleniumTestngTest extends BasicSetupTest {
 
+    @BeforeClass
+    public void setUp() {
+        // Инициализация браузера перед запуском всех тестов
+        browser = new ChromeDriver();
+    }
     @Test
     public void testABTesting() throws InterruptedException {
-        WebDriver browser = new ChromeDriver();
         browser.get("https://the-internet.herokuapp.com/abtest");
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content > div > h3")));
@@ -27,24 +33,16 @@ public class SeleniumTestngTest extends BasicSetupTest {
     }
     @Test
     public void testAddRemoveElements() throws InterruptedException {
-        WebDriver browser = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
         browser.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addButton = browser.findElement(By.cssSelector("#content > div > button"));
         for (int i = 0; i < 3; i++) {
-            WebElement addButton = browser.findElement(By.cssSelector("#content > div > button"));
             addButton.click();
-            Thread.sleep(1000); // Wait for button to be added
         }
         List<WebElement> deleteButtons = browser.findElements(By.cssSelector("#elements > button"));
         Assert.assertEquals(deleteButtons.size(), 3, "The number of 'Delete' buttons is not as expected!");
-        for (WebElement button : deleteButtons) {
-            Assert.assertTrue(button.isDisplayed(), "A 'Delete' button is not visible!");
-        }
-        while (deleteButtons.size() > 0) {
-            deleteButtons = browser.findElements(By.cssSelector("#elements > button"));
-            WebElement button = deleteButtons.get(0);
-            button.click();
-            Thread.sleep(1000);
+        deleteButtons.forEach(button -> Assert.assertTrue(button.isDisplayed(), "A 'Delete' button is not visible!"));
+        while (!deleteButtons.isEmpty()) {
+            deleteButtons.get(0).click();
             deleteButtons = browser.findElements(By.cssSelector("#elements > button"));
         }
             Assert.assertEquals(deleteButtons.size(), 0, "Some 'Delete' buttons are still visible!");
@@ -57,18 +55,15 @@ public class SeleniumTestngTest extends BasicSetupTest {
             if (!checkbox.isSelected()) {
                 checkbox.click();
             }
-            Assert.assertTrue(checkbox.isSelected(), "The checkbox is not vibranium.");
+            Assert.assertTrue(checkbox.isSelected(), "The checkbox is not selected.");
         }
     }
     @Test
     public void testDropdown() throws InterruptedException {
         browser.get("https://the-internet.herokuapp.com/dropdown");
         WebElement dropdown = browser.findElement(By.cssSelector("#dropdown"));
-        Thread.sleep(1000);
         Select select = new Select(dropdown);
-        Thread.sleep(1000);
         select.selectByVisibleText("Option 2");
-        Thread.sleep(1000);
         Assert.assertEquals(select.getFirstSelectedOption().getText(), "Option 2", "'Option 2' is not selected.");
     }
     @Test
@@ -76,18 +71,14 @@ public class SeleniumTestngTest extends BasicSetupTest {
         browser.get("https://the-internet.herokuapp.com/login");
         WebElement username = browser.findElement(By.cssSelector("#username"));
         WebElement password = browser.findElement(By.cssSelector("#password"));
-        WebElement loginButton = browser.findElement(By.cssSelector("#login > button > i"));
+        WebElement loginButton = browser.findElement(By.cssSelector("#login > button"));
         username.sendKeys("tomsmith");
-        Thread.sleep(1000);
         password.sendKeys("SuperSecretPassword!");
-        Thread.sleep(1000);
         loginButton.click();
         WebElement successMessage = browser.findElement(By.cssSelector("#flash"));
         Assert.assertTrue(successMessage.getText().contains("You logged into a secure area!"));
-        Thread.sleep(1000);
         WebElement logoutButton = browser.findElement(By.cssSelector("#content > div > a"));
         logoutButton.click();
-        Thread.sleep(1000);
         WebElement logoutMessage = browser.findElement(By.cssSelector("#flash"));
         Assert.assertTrue(logoutMessage.getText().contains("You logged out of the secure area!"));
     }
@@ -113,6 +104,10 @@ public class SeleniumTestngTest extends BasicSetupTest {
         Thread.sleep(2000);
         WebElement sliderValue = browser.findElement(By.cssSelector("#range"));
         Assert.assertNotEquals(sliderValue.getText(), "0");
+    }
+    @AfterClass
+    public void tearDown() {
+        browser.quit();
     }
 
         // Write the rest of TEST METHODS according to the task here, each method checking one scenario described in README.md file
